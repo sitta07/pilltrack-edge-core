197d16c20307a5dae3e32cae011a9408f1caa20a
sittalnwza007 <sittasahathum@gmail.com>
2025-12-23 17:14:05 +0700

    fix : remove his files to fix no prescription

--- test_yaml.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-try:
-    with open("config.yaml", "r") as f:
-        data = yaml.safe_load(f)
-        print("✅ YAML is OK!")
-        print(data)
-except Exception as e:
-    print(f"❌ YAML Error: {e}")
